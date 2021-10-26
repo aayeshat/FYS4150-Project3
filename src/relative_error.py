8914599d2937349f7a2722fd66c7a15733cfdca5
canaelgvin <canaelgvin@gmail.com>
2021-10-26 13:25:27 +0200

    rel error and conv

--- src/relative_error.py
+++ src/relative_error.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from glob import glob
+from scipy.stats import linregress
 
 q = 1   # Charge
 B0 = 96.5 # Magnetic field
@@ -28,12 +28,14 @@ A_neg = - (v_0 + w_pos * x_0) / (w_neg - w_pos) # A-
 #Analytical solutions
 
 
-filename_rk = ['../out/r_1(0.001).txt', '../out/r_1(0.01).txt', '../out/r_1(0.1).txt', '../out/r_1(0.05).txt', '../out/r_1(0.005).txt']
-h_name = ['0.001', '0.01', '0.1', '0.05', '0.005']
-filename_fe = ['../out/r_1_euler(0.001).txt','../out/r_1_euler(0.01).txt','../out/r_1_euler(0.1).txt','../out/r_1_euler(0.05).txt','../out/r_1_euler(0.005).txt']
+filename_rk = ['../out/r_1(0.1).txt', '../out/r_1(0.05).txt', '../out/r_1(0.01).txt', '../out/r_1(0.005).txt', '../out/r_1(0.001).txt']
+h_name = ['0.1', '0.05', '0.01', '0.005','0.001']
+filename_fe = ['../out/r_1_euler(0.1).txt','../out/r_1_euler(0.05).txt','../out/r_1_euler(0.01).txt','../out/r_1_euler(0.005).txt','../out/r_1_euler(0.001).txt']
 
 #Loop for plotting relative error for 5 different stepsizes h
 
+max_error_rk = []
+max_error_fe = []
 
 for i in range(5):
     x_rk, y_rk, z_rk = np.loadtxt(filename_rk[i], usecols = (1, 2, 3), unpack = True)
@@ -49,80 +51,64 @@ for i in range(5):
     r_rk = np.sqrt(x_rk**2 + y_rk**2 + z_rk**2)
     r_fe = np.sqrt(x_fe**2 + y_fe**2 + z_fe**2)
 
-    rel_err_rk = ( r_exc - r_rk ) / r_exc
-    rel_err_fe = ( r_exc - r_fe ) / r_exc
+    rel_err_rk = np.absolute( r_exc - r_rk ) / (r_exc)
+    rel_err_fe = np.absolute( r_exc - r_fe ) / (r_exc)
 
-    plt.plot(t, rel_err_rk, label = "h = " + h_name[i])
-    plt.legend()
+    max_rk = max(rel_err_rk)
+    max_fe = max(rel_err_fe)
+
+    max_error_rk.append(max_rk)
+    max_error_fe.append(max_fe)
+
+
+    # Plot RK solutions for different stepsizes
+
+    # plt.plot(t, rel_err_rk, label = "h = " + h_name[i])
     plt.ylabel('Relative error $\epsilon_i$')
     plt.xlabel('Time / $\mu$s')
-    plt.savefig('../out/relerr_rk.pdf')
+    # plt.savefig('../out/relerr_rk.pdf')
 
-    # plt.plot(t, rel_err_fe, label = "h = " + h_name[i])
-    # plt.savefig('../out/relerr_fe.pdf')
+    # Plot FE solutions for different stepsizes
 
-
-
-
-
-    #Load x y z columns from output files
-
-#Load x y z output from RK
-# fname_rk = '../out/r_xy_nointer_1_2.txt'
-# x, y, z = np.loadtxt(fname_rk, usecols = (1,2,3), unpack = True)
-#
-#
-#
-# #Relative error RK
-# rel_err = ( r_exc - r_rk ) / r_exc
-#
-# #Load x y z output from FE
-# fname_euler = '../out/r_xy_nointer_1_2_euler.txt'
-# x_fe, y_fe, z_fe = np.loadtxt(fname_euler, usecols = (1,2,3), unpack = True)
-
-#Relative error FE
+    plt.plot(t, rel_err_fe, label = "h = " + h_name[i])
+    plt.legend()
+    plt.savefig('../out/relerr_fe.pdf')
 
 
-# plt.plot(t, y_exc, label = 'y, exact')
-# plt.plot(t, y, label = 'y, RK')
-# plt.plot(t, x_exc, label = 'x, exc')
-# plt.plot(t, x, label = 'x, RK')
-# plt.plot(t, z_exc, label = 'z, exc')
-# plt.plot(t, z, label = "z, rk")
+#Plot single numerical solution
+
+# x_rk, y_rk, z_rk = np.loadtxt('../out/r_1_euler(0.001).txt', usecols = (1, 2, 3), unpack = True)
+# plt.plot(t,x_rk, label = 'x')
+# plt.plot(t,y_rk, label = 'y')
+# plt.plot(t,z_rk, label = 'z')
+# plt.ylabel('x, y, z / $\mu$m')
+# plt.xlabel('time / $\mu$s')
 # plt.legend()
-#
-#
-# plt.plot(t, z)
-#
-#
-# # Plotting relative error
-# plt.plot(t, rel_err, label = 'RK')
-# plt.plot(t, rel_err_fe, label = 'FE')
+# plt.savefig('../out/numerical_solutions.pdf')
+# plt.show()
+
+h = [0.1, 0.05, 0.01, 0.005, 0.001]
+
+r_err_rk = []
+r_err_fe = []
+
+
+for k in range(1,5):
+    h_den = (np.log10(h[k] / h[k-1]))
+
+    rk = 0.25 * np.log10( max_error_rk[k] / max_error_rk[k-1] ) / h_den
+    r_err_rk.append(rk)
+
+    fe = 0.25 * np.log10( max_error_fe[k] / max_error_fe[k-1] ) / h_den
+    r_err_fe.append(fe)
+
+# Plot estimation of error convergence
+
+# plt.plot(r_err_rk, marker = 'o', label = 'RK')
+# plt.plot(r_err_fe, marker = 'o', label = 'FE')
+# plt.xlabel('k - 2')
+# plt.ylabel('$r_{err}$')
+# plt.xticks([0,1,2,3,])
 # plt.legend()
-# plt.ylabel('Relative error')
-# plt.xlabel('t')
-# #plt.savefig('../out/rel_err.pdf')
-#
-#
-# plt.plot(t, z_exc, label = 'exc')
-# plt.plot(t, z, label = 'rk')
-# plt.legend()
-#
-#
-# #Plotting analytical solutions
-# plt.plot(t, x_exc, label = 'x')
-# plt.plot(t, y_exc, label = 'y')
-# plt.plot(t, z_exc, label = 'z')
-# plt.ylabel('x,y,z')
-# plt.xlabel('t')
-# plt.legend()
-# #plt.savefig('../out/analytical_solutions.pdf')
-#
-# #Plotting numerical solutions
-# plt.plot(t, x, label = 'x')
-# plt.plot(t, y, label = 'y')
-# plt.plot(t, z, label = 'z')
-# plt.ylabel('x,y,z')
-# plt.xlabel('t')
-# plt.legend()
-#plt.savefig('../out/numerical_solutions.pdf')
+#plt.savefig('../out/error_conv.pdf')
+# plt.show()
